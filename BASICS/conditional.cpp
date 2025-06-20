a2a407824dd7cb58b6694c62b93d7aca64d07754
Arpita Mahapatra <lipsha2712@gmail.com>
2025-06-20 18:40:57 +0530

    till loops

---
+++ BASICS/conditional.cpp
@@ -0,0 +1,46 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n;
+cout << "enter ur no madame";
+cin>> n;
+    if (n>=0)
+    {
+       cout << "no is positive";
+    }
+    else
+    cout<< "no is neg";
+    return 0;
+}
+    
+// find lower case upper case
+#include<iostream>
+using namespace std;
+int main(){
+    char n;
+cout << "enter ur character";
+cin>> n;
+    if ('a'<= n&& n<='z')
+    {
+       cout << "lowercase";
+    }
+    else if('A'<=n && n<='Z'){
+    cout<< "uppercase";}
+    else
+    cout << "invalid input";
+    return 0;
+}
+    */
+   #include<iostream>
+using namespace std;
+int main(){
+   int sum=0,n;
+   cout << "state ur no";
+   cin>> n;
+   for (int i = 0; i <=n; i++)
+   {
+     cout<<
+   }
+   
+    return 0;
+}
