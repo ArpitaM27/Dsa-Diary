63defd2f768a872affb967244cbcd7269ff993ba
Arpita Mahapatra <lipsha2712@gmail.com>
2025-06-27 00:34:57 +0530

    remove

--- BASICS/patterns.cpp
+++ BASICS/patterns.cpp
@@ -84,3 +84,27 @@ for (int j = i; j <n; j++)
 }
 return 0;
 }
+
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+cout<< "state ur no";
+cin>>n;
+for (int i = 1; i <= n; i++)
+{
+   
+   for (int j = 1; j <=n-i; j++)
+   {
+    cout<< " ";
+   }
+  
+
+for (int j = i; j <=2*i-1; j++)
+{
+   cout << "*" ;
+}
+ cout<<endl;
+}
+return 0;
+}
