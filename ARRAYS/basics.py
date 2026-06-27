67b9248b9fb145be66f45eda5adb9d9b8524ef41
Arpita Mahapatra <lipsha2712@gmail.com>
2026-06-27 23:32:50 +0530

    Started arrays

---
+++ ARRAYS/basics.py
@@ -0,0 +1,35 @@
+import array
+
+# val=array.array('i',[1,2,3,4,5])
+
+# for i in range(len(val)):
+#     print(val[i],end=" ")
+# print("\n")
+# val.reverse()
+# for i in range(len(val))
+#     print(val[i],end=" , ")
+
+# val.append(6)
+# val.insert(0,6)
+# val[2]=100
+# print(val)
+
+# copyarray=array.array(val.typecode, (y*3 for y in val))
+# for i in range(len(copyarray)):
+#     print(copyarray[i],end=" ")
+
+# abc=val[0:4]
+# abc=val[::-1]
+# for i in range(len(abc)):
+#     print(abc[i],end=" ")
+
+# from array import *
+# arr=array('i',[])
+# n=int(input("Enter the length of array: "))
+
+for i in range(n):
+    x=int(input("Enter the next value: "))
+    arr.append(x)
+
+for j in range(n):
+    print(arr[j],end=" ")
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# C-Chronicles
-Learning C++
+ Data Structures & Algorithms in Python
+
+This repository contains my solutions to Data Structures and Algorithms (DSA) problems solved using Python. It serves as a record of my learning journey, coding practice, and interview preparation.
+
+## Objectives
+
+- Strengthen problem-solving skills
+- Build a strong foundation in DSA
+- Prepare for coding interviews and online assessments
+- Improve code quality and algorithmic thinking
+- Practice consistent daily problem solving
