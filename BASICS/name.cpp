0e03bfdae5cace41e9eee150b2fdb60ef2b99895
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-03 20:25:14 +0530

    deleted cpp folder,made all sorting algo files

--- BASICS/name.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-
-cout<<"Arpita\n" << "Mahapatra\n";return 0;
-}
--- BASICS/sum.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-int a,b;
-cout<< "enter you nos";
-cin>> a>> b;
-int sum=a+b;
-cout<< "sum: \n" << sum;
-}
