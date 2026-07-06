b17fcb066420c73430e6c39edfc284663bced692
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-06 22:04:46 +0530

    Leetcode 0268

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -1,12 +1,21 @@
+# class Solution:
+#     def check(self, nums):
+#         count = 0
+
+#         for i in range(len(nums)):
+#             if nums[i] > nums[(i + 1) % len(nums)]:
+#                 count += 1
+
+#         if count <= 1:
+#             return True
+#         else:
+#             return False
+        
 class Solution:
-    def check(self, nums):
-        count = 0
+    def missingNumber(self, nums):
+        n = len(nums)
 
-        for i in range(len(nums)):
-            if nums[i] > nums[(i + 1) % len(nums)]:
-                count += 1
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
 
-        if count <= 1:
-            return True
-        else:
-            return False
+        return expected_sum - actual_sum
