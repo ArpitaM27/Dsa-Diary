b37ed99c6f84a4bddf696cf908a1f10b871281d5
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-09 20:15:25 +0530

    leetcode 0035

--- Demo_Practice/array.py
+++ Demo_Practice/array.py
@@ -1,16 +1,34 @@
 # Given an array nums, return true if the array was originally sorted in non-decreasing order, 
 # then rotated some number of positions (including zero). Otherwise, return false.
 
-print("Enter the array elements separated by space:")
-nums=list(map(int,input().split()))
+# print("Enter the array elements separated by space:")
+# nums=list(map(int,input().split()))
 
-count = 0
+# count = 0
 
-for i in range(len(nums)):
-    if nums[i] > nums[(i + 1) % len(nums)]:
-        count += 1
+# for i in range(len(nums)):
+#     if nums[i] > nums[(i + 1) % len(nums)]:
+#         count += 1
 
-if count <= 1:
-    print(True)
-else:
-    print(False)
+# if count <= 1:
+#     print(True)
+# else:
+#     print(False)
+
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+class Solution:
+    def searchInsert(self, nums, target):
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                low=mid+1
+            else:
+                high=mid-1
+        return low
