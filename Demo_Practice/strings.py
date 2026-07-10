7656563d1c575c285a8bb5fb8ce13b16de739b9e
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-10 20:20:30 +0530

    leetcode 0796

--- Demo_Practice/strings.py
+++ Demo_Practice/strings.py
@@ -4,16 +4,32 @@
 
 
 
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        if not strs:
-            return ""
-        shortest = min(strs, key=len)
-        for i in range(len(shortest)):
-            for string in strs:
-                if string[i] != shortest[i]:
-                    if i == 0:
-                        return ""
-                    return shortest[:i]
+# class Solution(object):
+#     def longestCommonPrefix(self, strs):
+#         if not strs:
+#             return ""
+#         shortest = min(strs, key=len)
+#         for i in range(len(shortest)):
+#             for string in strs:
+#                 if string[i] != shortest[i]:
+#                     if i == 0:
+#                         return ""
+#                     return shortest[:i]
 
-        return shortest
+#         return shortest  
+
+# Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+# A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+# For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+class Solution(object):
+    def rotateString(self, s, goal):
+        if len(s) !=len(goal):
+            return False
+        if s in goal+goal:
+            return True
+        return False
+        
