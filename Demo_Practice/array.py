8519879f2098390914fa02ec7bd187920df6475c
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-11 12:09:13 +0530

    leetcode 0303

--- Demo_Practice/array.py
+++ Demo_Practice/array.py
@@ -66,9 +66,38 @@
 #         return max_counts
 
 # Given a non-empty array of integers nums, every element appears twice except for one. Find that single one
-class Solution(object):
-    def singleNumber(self, nums):
-        result=0
-        for i in range(len(nums)):
-            result=result^nums[i]
-        return result
+# class Solution(object):
+#     def singleNumber(self, nums):
+#         result=0
+#         for i in range(len(nums)):
+#             result=result^nums[i]
+#         return result 
+
+# Given an integer array nums, handle multiple queries of the following type:
+
+# Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+# Implement the NumArray class:
+
+# NumArray(int[] nums) Initializes the object with the integer array nums.
+# int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]). 
+
+# class NumArray(object):
+
+#     def __init__(self, nums):
+        
+#         self.prefix_sum = [0] * (len(nums))
+#         self.prefix_sum[0] = nums[0]
+#         for i in range(1, len(nums)):
+#             self.prefix_sum[i] = self.prefix_sum[i - 1] + nums[i]
+
+    
+#     def sumRange(self, left, right):
+#         if left == 0:
+#             return self.prefix_sum[right]
+#         return self.prefix_sum[right] - self.prefix_sum[left - 1]
+      
+    
+        
+    
+        
+     
