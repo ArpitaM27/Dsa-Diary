c51878e6def0e9e54c27110b69ee0decce0c4dd2
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-11 13:08:29 +0530

    leetcode 1732

--- Demo_Practice/array.py
+++ Demo_Practice/array.py
@@ -102,11 +102,26 @@
 
 # Return the running sum of nums.
 
+# class Solution(object):
+#     def runningSum(self, nums):
+#         for i in range(1, len(nums)):
+#             nums[i] += nums[i - 1]
+#         return nums
+
+
+# There is a biker going on a road trip. The road trip consists of n + 1 points at various altitudes. The biker starts his trip on point 0 with altitude equal 0.
+
+# You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+   
 class Solution(object):
-    def runningSum(self, nums):
-        for i in range(1, len(nums)):
-            nums[i] += nums[i - 1]
-        return nums
+    def largestAltitude(self, gain):
+        max_altitude = 0
+        current_altitude = 0
+        for g in gain:
+            current_altitude += g
+            max_altitude = max(max_altitude, current_altitude)
+        return max_altitude
 
 
 
