ff267d2d11f308c5c1af8f22bba111bfd7aa2b61
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-14 14:22:58 +0530

    leetcode 876

--- Demo_Practice/linked_list.py
+++ Demo_Practice/linked_list.py
@@ -4,22 +4,37 @@
 #         self.val = val
 #         self.next = next
 
+# class Solution(object):
+#     def removeElements(self, head, val):
+
+#         dummy = ListNode(0)
+#         dummy.next = head
+
+#         prev = dummy
+#         curr = head
+
+#         while curr is not None:
+
+#             if curr.val == val:
+#                 prev.next = curr.next
+#             else:
+#                 prev = curr
+
+#             curr = curr.next
+
+#         return dummy.next   
+
+# Given the head of a singly linked list, return the middle node of the linked list.
+
+# If there are two middle nodes, return the second middle node.
+
 class Solution(object):
-    def removeElements(self, head, val):
-
-        dummy = ListNode(0)
-        dummy.next = head
-
-        prev = dummy
-        curr = head
-
-        while curr is not None:
-
-            if curr.val == val:
-                prev.next = curr.next
-            else:
-                prev = curr
-
-            curr = curr.next
-
-        return dummy.next
+    def middleNode(self, head):
+        slow=head
+        fast=head
+        
+        while(fast!=None and fast.next!=None):
+           
+             slow=slow.next
+             fast=fast.next.next
+        return slow
