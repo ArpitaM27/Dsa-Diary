fe3c4a0d1cb8a99bee4347d79568a31bc9b4ca48
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-14 16:09:23 +0530

    leetcode 237

--- Demo_Practice/linked_list.py
+++ Demo_Practice/linked_list.py
@@ -94,3 +94,24 @@ class Solution(object):
              if(slow==fast):
                   return False
          
+# There is a singly-linked list head and we want to delete a node node in it.
+
+# You are given the node to be deleted node. You will not be given access to the first node of head.
+
+# All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+
+# Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:
+
+# The value of the given node should not exist in the linked list.
+# The number of nodes in the linked list should decrease by one.
+# All the values before node should be in the same order.
+# All the values after node should be in the same order.  
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution(object):
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node.next.next
