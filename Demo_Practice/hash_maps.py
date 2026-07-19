07c4fbd4935d4e9fd4e4450b99c603cd473dc595
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-19 13:49:50 +0530

    leetcode 242

--- Demo_Practice/hash_maps.py
+++ Demo_Practice/hash_maps.py
@@ -22,3 +22,21 @@ class Solution(object):
             seen.add(x)
 
         return False
+
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        d1={}
+        d2={}
+        for x in t:
+            if x in d1:
+                d1[x]+=1
+            else:
+                d1[x]=1
+        for y in s:
+            if y in d2:
+                d2[y]+=1
+            else:
+                d2[y]=1
+        return d1==d2
