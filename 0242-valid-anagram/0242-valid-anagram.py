6355b7f45549b3b5f3e70a75531d252195c65391
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-19 13:53:49 +0530

    Time: 15 ms (86.68%), Space: 12.5 MB (85.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,13 +3,8 @@ class Solution(object):
         d1={}
         d2={}
         for x in t:
-            if x in d1:
-                d1[x]+=1
-            else:
-                d1[x]=1
+                d1[x]=d1.get(x,0)+1
         for y in s:
-            if y in d2:
-                d2[y]+=1
-            else:
-                d2[y]=1
+                d2[y]=d2.get(y,0)+1
+                
         return d1==d2
