d95fd6977ecba13eb213abd8ea4ca5acb06bc954
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-21 11:54:44 +0530

    leetcode 27

--- Demo_Practice/hash_maps.py
+++ Demo_Practice/hash_maps.py
@@ -69,4 +69,37 @@ class Solution(object):
             d[key].append(x)
         return list(d.values())
         
-   
+# Given two strings s and t, determine if they are isomorphic.
+
+# Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+# All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+
+        if len(s) != len(t):
+            return False
+
+        s_to_t = {}
+        t_to_s = {}
+
+        for i in range(len(s)):
+
+            # If mapping already exists, it must match
+            if s[i] in s_to_t:
+                if s_to_t[s[i]] != t[i]:
+                    return False
+            else:
+                s_to_t[s[i]] = t[i]
+
+            # Reverse mapping must also match
+            if t[i] in t_to_s:
+                if t_to_s[t[i]] != s[i]:
+                    return False
+            else:
+                t_to_s[t[i]] = s[i]
+
+        return True
+    (lc 205 solve again)
+    
