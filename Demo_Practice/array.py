be36de10340c0dda60c25b3b0bf5fc2d3abff155
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-22 23:44:02 +0530

    leetcode 392

--- Demo_Practice/array.py
+++ Demo_Practice/array.py
@@ -234,3 +234,11 @@ class Solution(object):
            
        return slow
    
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+# A word is a maximal substring consisting of non-space characters only.  
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        s=s.split()
+        return len(s[-1])
