b2e3ec0388b31addb4551c4f8976fc1e759311b3
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-25 15:08:49 +0530

    leetcode 231

--- Demo_Practice/bits.py
+++ Demo_Practice/bits.py
@@ -12,14 +12,30 @@
 #  Given a positive integer n, write a 
 #  function that returns the number of set bits in its binary representation (also known as the Hamming weight).
 
-class Solution(object):
-    def hammingWeight(self, n):
-        count=0
-        while n:
-            n=n & (n-1)
-            count+=1
-        return count
+# class Solution(object):
+#     def hammingWeight(self, n):
+#         count=0
+#         while n:
+#             n=n & (n-1)
+#             count+=1
+#         return count
 
+# obj=Solution()
+# n=2147483645
+# print(obj.hammingWeight(n)) 
+
+
+# Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+# An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if n <= 0:
+            return False
+
+        return (n & (n - 1)) == 0
+    
 obj=Solution()
-n=2147483645
-print(obj.hammingWeight(n))
+n=0
+print(obj.isPowerOfTwo(n))
