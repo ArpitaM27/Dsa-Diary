dbb3251302114e0821eb717151045aa465288c2d
Arpita Mahapatra <lipsha2712@gmail.com>
2026-07-30 10:09:05 +0530

    leetcode 1

--- Demo_Practice/hash_maps.py
+++ Demo_Practice/hash_maps.py
@@ -101,3 +101,19 @@
 #               return True   
 #          d[nums[i]]=i   
 #      return False
+
+# You are given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        d={}
+        for i in range(len(nums)):
+                need=target-nums[i]
+                if need in d:
+                  return[i,d[need]]
+                d[nums[i]]=i
+        return Falses
