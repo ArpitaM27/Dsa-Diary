64ac997d577a0a985015c92d18de19885c7da4ac
Arpita Mahapatra <lipsha2712@gmail.com>
2026-08-01 11:16:39 +0530

    leetcode 2390

--- Demo_Practice/strings.py
+++ Demo_Practice/strings.py
@@ -164,3 +164,30 @@
 #                 return t[i]
 
 #         return t[-1]
+# You are given a string s, which contains stars *.
+
+# In one operation, you can:
+
+# Choose a star in s.
+# Remove the closest non-star character to its left, as well as remove the star itself.
+# Return the string after all stars have been removed.
+
+
+
+class Solution(object):
+    def removeStars(self, s):
+        stack=[]
+        for x in s:
+            if x!="*":
+                stack.append(x)
+            if x=="*":
+                
+                stack.pop()
+        return "".join(stack)
+    
+obj=Solution()
+s="leet**cod*e"
+print(obj.removeStars(s))
+                
+            
+         
