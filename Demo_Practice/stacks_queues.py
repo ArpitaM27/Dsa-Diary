74d28a8f0078cc9487808658db5e420585ec1d4c
Arpita Mahapatra <lipsha2712@gmail.com>
2026-08-08 20:26:58 +0530

    lc 21

--- Demo_Practice/stacks_queues.py
+++ Demo_Practice/stacks_queues.py
@@ -151,14 +151,16 @@
 #     array answer such that answer[i] is the number of days you have to wait after the ith day to get 
 #     a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
 
-class Solution(object):
-    def dailyTemperatures(self, temperatures):
-      arr=[0]*len(temperatures)
-      stack=[]
-      i=0
-      for i in range(len(temperatures)):
-          while stack and temperatures[i]>temperatures[stack[-1]]:
-              distance=i-(stack[-1])
-              arr[stack.pop()]=distance
-          stack.append(i)
-      return arr
+# class Solution(object):
+#     def dailyTemperatures(self, temperatures):
+#       arr=[0]*len(temperatures)
+#       stack=[]
+#       i=0
+#       for i in range(len(temperatures)):
+#           while stack and temperatures[i]>temperatures[stack[-1]]:
+#               distance=i-(stack[-1])
+#               arr[stack.pop()]=distance
+#           stack.append(i)
+#       return arr                            
+
+
