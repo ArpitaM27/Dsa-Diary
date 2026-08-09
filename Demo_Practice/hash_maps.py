6095556b8e86d326d8e0afeb32ae1320d1a58cea
Arpita Mahapatra <lipsha2712@gmail.com>
2026-08-10 00:38:52 +0530

    leetcode 28

--- Demo_Practice/hash_maps.py
+++ Demo_Practice/hash_maps.py
@@ -157,27 +157,37 @@
 #                     break
 #         return arr
            
-class Solution(object):
-    def wordPattern(self, pattern, s):
-        d1 = {}
-        d2 = {}
-        s = s.split()
+# class Solution(object):
+#     def wordPattern(self, pattern, s):
+#         d1 = {}
+#         d2 = {}
+#         s = s.split()
 
-        if len(pattern) != len(s):
-            return False
+#         if len(pattern) != len(s):
+#             return False
 
-        for i in range(len(pattern)):
+#         for i in range(len(pattern)):
 
-            if pattern[i] in d1:
-                if d1[pattern[i]] != s[i]:
-                    return False
-            else:
-                d1[pattern[i]] = s[i]
+#             if pattern[i] in d1:
+#                 if d1[pattern[i]] != s[i]:
+#                     return False
+#             else:
+#                 d1[pattern[i]] = s[i]
 
-            if s[i] in d2:
-                if d2[s[i]] != pattern[i]:
-                    return False
-            else:
-                d2[s[i]] = pattern[i]
+#             if s[i] in d2:
+#                 if d2[s[i]] != pattern[i]:
+#                     return False
+#             else:
+#                 d2[s[i]] = pattern[i]
 
-        return True
+#         return True
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+   
+# class Solution(object):
+#     def strStr(self, haystack, needle):
+#         for i in range(len(haystack) - len(needle) + 1):
+#             if haystack[i : i + len(needle)]==needle:
+#                 return i
+#         return -1       
+            
