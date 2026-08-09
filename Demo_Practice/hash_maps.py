79b6686d4b948879865e22f53add8a5c4c121098
Arpita Mahapatra <lipsha2712@gmail.com>
2026-08-09 23:49:21 +0530

    leetcode 290

--- Demo_Practice/hash_maps.py
+++ Demo_Practice/hash_maps.py
@@ -157,3 +157,27 @@
 #                     break
 #         return arr
            
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        d1 = {}
+        d2 = {}
+        s = s.split()
+
+        if len(pattern) != len(s):
+            return False
+
+        for i in range(len(pattern)):
+
+            if pattern[i] in d1:
+                if d1[pattern[i]] != s[i]:
+                    return False
+            else:
+                d1[pattern[i]] = s[i]
+
+            if s[i] in d2:
+                if d2[s[i]] != pattern[i]:
+                    return False
+            else:
+                d2[s[i]] = pattern[i]
+
+        return True
