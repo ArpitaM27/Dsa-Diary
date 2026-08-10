81fd5c139b43b0fc607d02a20a7db830fa1ac7ba
Arpita Mahapatra <lipsha2712@gmail.com>
2026-08-10 16:40:59 +0530

    leetcode 973

--- Demo_Practice/heap.py
+++ Demo_Practice/heap.py
@@ -29,18 +29,42 @@
 
 # Return the weight of the last remaining stone. If there are no stones left, return 0.
 
-import heapq
-class Solution(object):
-    def lastStoneWeight(self, stones):
-        heap=[]
-        for x in stones:
-            heapq.heappush(heap,-x)
-        while len(heap) > 1:
-                a=-heapq.heappop(heap)
-                b=-heapq.heappop(heap)
-                if a != b:
-                 heapq.heappush(heap, -(a-b))
-        if heap:
-            return -heap[0]
+# import heapq
+# class Solution(object):
+#     def lastStoneWeight(self, stones):
+#         heap=[]
+#         for x in stones:
+#             heapq.heappush(heap,-x)
+#         while len(heap) > 1:
+#                 a=-heapq.heappop(heap)
+#                 b=-heapq.heappop(heap)
+#                 if a != b:
+#                  heapq.heappush(heap, -(a-b))
+#         if heap:
+#             return -heap[0]
 
-        return 0
+#         return 0
+
+# Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the k closest points to the origin (0, 0).
+
+# The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+
+# You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+import heapq
+
+class Solution(object):
+    def kClosest(self, points, k):
+        heap = []
+
+        for point in points:
+            x = point[0]
+            y = point[1]
+
+            distance = x*x + y*y
+
+            heapq.heappush(heap, (-distance, point))
+
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [point for distance, point in heap]
