904712c9292dfcec9315810bcd94819ad7af8e29
Arpita Mahapatra <lipsha2712@gmail.com>
2026-08-12 18:44:41 +0530

    leetcode 57

---
+++ Demo_Practice/intervals.py
@@ -0,0 +1,31 @@
+# You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+
+# Two intervals are considered overlapping if they share at least one point.
+
+# Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+
+# Return intervals after the insertion.
+
+# Note that you don't need to modify intervals in-place. You can make a new array and return it.    
+
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        result = []
+
+        for interval in intervals:
+
+            if interval[1] < newInterval[0]:
+                result.append(interval)
+
+            elif interval[0] > newInterval[1]:
+                result.append(newInterval)
+                newInterval = interval
+
+            else:
+                newInterval[0] = min(newInterval[0], interval[0])
+                newInterval[1] = max(newInterval[1], interval[1])
+
+        result.append(newInterval)
+
+        return result
