55fdb89a86e6e745a0f79e2f312939556f3e9427
Arpita Mahapatra <lipsha2712@gmail.com>
2026-08-12 19:33:02 +0530

    leetcode 56

--- Demo_Practice/intervals.py
+++ Demo_Practice/intervals.py
@@ -9,23 +9,48 @@
 # Note that you don't need to modify intervals in-place. You can make a new array and return it.    
 
 
-class Solution(object):
-    def insert(self, intervals, newInterval):
-        result = []
+# class Solution(object):
+#     def insert(self, intervals, newInterval):
+#         result = []
 
-        for interval in intervals:
+#         for interval in intervals:
 
-            if interval[1] < newInterval[0]:
-                result.append(interval)
+#             if interval[1] < newInterval[0]:
+#                 result.append(interval)
 
-            elif interval[0] > newInterval[1]:
-                result.append(newInterval)
-                newInterval = interval
+#             elif interval[0] > newInterval[1]:
+#                 result.append(newInterval)
+#                 newInterval = interval
 
-            else:
-                newInterval[0] = min(newInterval[0], interval[0])
-                newInterval[1] = max(newInterval[1], interval[1])
+#             else:
+#                 newInterval[0] = min(newInterval[0], interval[0])
+#                 newInterval[1] = max(newInterval[1], interval[1])
 
-        result.append(newInterval)
+#         result.append(newInterval)
 
-        return result
+#         return result
+
+
+# Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+# class Solution(object):
+#     def merge(self, intervals):
+#         intervals.sort()
+
+#         result = []
+#         result.append(intervals[0])
+
+#         for interval in intervals[1:]:
+#             start = interval[0]
+#             end = interval[1]
+
+#             last = result[-1]
+
+#             if start <= last[1]:
+#                 last[1] = max(last[1], end)
+#             else:
+#                 result.append([start, end])
+
+#         return result
+
+
