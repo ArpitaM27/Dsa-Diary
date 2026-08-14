a6a049ace5e1e0b293060e7adbf20612cf6013d6
Arpita Mahapatra <lipsha2712@gmail.com>
2026-08-14 18:40:39 +0530

    leetcode 34

--- Demo_Practice/array.py
+++ Demo_Practice/array.py
@@ -554,3 +554,56 @@
 #                 left = mid + 1
 
 #         return ans
+
+
+# Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+# If target is not found in the array, return [-1, -1].
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+
+class Solution(object):
+    def searchRange(self, nums, target):
+
+        def first():
+            left = 0
+            right = len(nums) - 1
+            ans = -1
+
+            while left <= right:
+                mid = (left + right) // 2
+
+                if nums[mid] == target:
+                    ans = mid
+                    right = mid - 1
+
+                elif nums[mid] < target:
+                    left = mid + 1
+
+                else:
+                    right = mid - 1
+
+            return ans
+
+        def last():
+            left = 0
+            right = len(nums) - 1
+            ans = -1
+
+            while left <= right:
+                mid = (left + right) // 2
+
+                if nums[mid] == target:
+                    ans = mid
+                    left = mid + 1
+
+                elif nums[mid] < target:
+                    left = mid + 1
+
+                else:
+                    right = mid - 1
+
+            return ans
+
+        return [first(), last()] 
