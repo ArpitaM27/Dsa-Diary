d9f60033d132ee818195b7ff37b9850fe6088d49
Arpita Mahapatra <lipsha2712@gmail.com>
2026-08-14 19:00:50 +0530

    leetcode 7

--- Demo_Practice/array.py
+++ Demo_Practice/array.py
@@ -563,47 +563,47 @@
 # You must write an algorithm with O(log n) runtime complexity.
 
 
-class Solution(object):
-    def searchRange(self, nums, target):
+# class Solution(object):
+    # def searchRange(self, nums, target):
 
-        def first():
-            left = 0
-            right = len(nums) - 1
-            ans = -1
+    #     def first():
+    #         left = 0
+    #         right = len(nums) - 1
+    #         ans = -1
 
-            while left <= right:
-                mid = (left + right) // 2
+    #         while left <= right:
+    #             mid = (left + right) // 2
 
-                if nums[mid] == target:
-                    ans = mid
-                    right = mid - 1
+    #             if nums[mid] == target:
+    #                 ans = mid
+    #                 right = mid - 1
 
-                elif nums[mid] < target:
-                    left = mid + 1
+    #             elif nums[mid] < target:
+    #                 left = mid + 1
 
-                else:
-                    right = mid - 1
+    #             else:
+    #                 right = mid - 1
 
-            return ans
+    #         return ans
 
-        def last():
-            left = 0
-            right = len(nums) - 1
-            ans = -1
+    #     def last():
+    #         left = 0
+    #         right = len(nums) - 1
+    #         ans = -1
 
-            while left <= right:
-                mid = (left + right) // 2
+    #         while left <= right:
+    #             mid = (left + right) // 2
 
-                if nums[mid] == target:
-                    ans = mid
-                    left = mid + 1
+    #             if nums[mid] == target:
+    #                 ans = mid
+    #                 left = mid + 1
 
-                elif nums[mid] < target:
-                    left = mid + 1
+    #             elif nums[mid] < target:
+    #                 left = mid + 1
 
-                else:
-                    right = mid - 1
+    #             else:
+    #                 right = mid - 1
 
-            return ans
+    #         return ans
 
-        return [first(), last()] 
+    #     return [first(), last()] 
--- Demo_Practice/strings.py
+++ Demo_Practice/strings.py
@@ -190,4 +190,22 @@
 # print(obj.removeStars(s))
                 
             
-         
+# Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+# Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+class Solution(object):
+    def reverse(self, x):
+        total = 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        while x != 0:
+            digit = x % 10
+            total = total * 10 + digit
+            x = x // 10
+
+        if total > 2147483647:
+            return 0
+
+        return sign * total
