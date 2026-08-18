9134c9ec502dcfc39fdfd20428a8792517f0f5d6
Arpita Mahapatra <lipsha2712@gmail.com>
2026-08-18 23:38:08 +0530

    Time: 16 ms (64.67%), Space: 12.3 MB (88.68%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseBits(self, n):
+        ans = 0
+
+        for i in range(32):
+            bit = n & 1
+            ans = (ans << 1) | bit
+            n = n >> 1
+
+        return ans
+        
