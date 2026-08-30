8b24495bed760f687bfbf4c09f83c7acd1faca7e
Arpita Mahapatra <lipsha2712@gmail.com>
2026-08-30 10:46:48 +0530

    Time: 70 ms (31.74%), Space: 4.1 MB (59.8%) - LeetHub

---
+++ 0194-transpose-file/0194-transpose-file.sh
@@ -0,0 +1,10 @@
+# Read from the file file.txt and print its transposed content to stdout.
+awk '
+{
+    for (i = 1; i <= NF; i++)
+        a[i] = a[i] ? a[i] FS $i : $i
+}
+END {
+    for (i = 1; i <= NF; i++)
+        print a[i]
+}' file.txt
